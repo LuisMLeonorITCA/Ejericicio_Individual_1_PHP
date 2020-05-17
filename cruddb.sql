9014cb5fd066fceb2db1bcd89c9eba711321bc9b
LuisMLeonorITCA <64717459+LuisMLeonorITCA@users.noreply.github.com>
2020-05-16 21:55:51 -0600

    Tercer Commit subida de CRUD Testing

---
+++ cruddb.sql
@@ -0,0 +1,15 @@
+﻿
+
+use SIS11BC2;
+
+create table Menus (
+Id_menus int not null primary key identity (1,1),
+platillo varchar (20) not null,
+cantidad int not null,
+precio money not null
+);
+
+insert into Menus (platillo, cantidad, precio)
+values('pupusas', 3, 0.50),  
+        ('sopa de caracol' , 1 , 3.50);
+		select * from Menus;
